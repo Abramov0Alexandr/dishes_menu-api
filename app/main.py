33bc5689bb1afe21bdf14e60c18b6541ab81a7e6
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2024-01-20 01:45:19 +0300

    First test run

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+from app import app_settings
+
+project_data = app_settings.project_detail_information
+
+
+app = FastAPI(**project_data)
+
+
+@app.get('/api/')
+async def hello():
+    return {'message': 'Hello, world!'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
