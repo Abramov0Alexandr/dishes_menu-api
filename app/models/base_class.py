12d75a0fdd55d58ecc893e71bf91c78bfa0e4364
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2024-01-21 13:21:01 +0300

    Modified "id" type from int to UUID

--- app/models/base_class.py
+++ app/models/base_class.py
@@ -1,9 +1,14 @@
+from abc import ABCMeta
+from dataclasses import dataclass
+
+from pydantic import Field, UUID4
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import as_declarative
+from sqlalchemy.orm import as_declarative, MappedAsDataclass, registry
 from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from uuid import UUID, uuid4
 
 
 metadata = MetaData()
@@ -11,7 +16,7 @@ metadata = MetaData()
 
 @as_declarative()
 class Base:
-    id: int
+    id: UUID
     __name__: str
     metadata = MetaData()
 
@@ -23,6 +28,6 @@ class Base:
 class BaseModel(Base):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, index=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
